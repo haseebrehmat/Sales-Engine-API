7c2af03ebaa615e5b7218cd8f22a5301357ccace
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-01-25 16:05:47 +0500

    project setup

---
+++ settings/base.py
@@ -0,0 +1,128 @@
+import environ
+import os
+
+import os
+from pathlib import Path
+import environ
+
+root = environ.Path("env")
+env = environ.Env(DEBUG=(bool, False),)     # set default values and casting
+environ.Env.read_env()                      # reading .env file
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# False if not in os.environ because of casting above
+
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = ["*"]
+
+
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+CUSTOM_APPS = []
+
+THIRD_PARTY_APPS = []
+
+INSTALLED_APPS += CUSTOM_APPS + THIRD_PARTY_APPS
+
+# Defining Middlewares
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CUSTOM_MIDDLEWARES = []
+
+THRID_PARTY_MIDDLEWARES = []
+
+MIDDLEWARE += CUSTOM_MIDDLEWARES + THRID_PARTY_MIDDLEWARES
+
+ROOT_URLCONF = 'settings.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'settings.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ settings/production.py
@@ -0,0 +1,12 @@
+from settings.base import *
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': env("PRODUCTION_DB_ENGINE"),
+        'NAME': env("PRODUCTION_DB_NAME"),
+        'USER': env("PRODUCTION_DB_USER"),
+        'PASSWORD': env("PRODUCTION_DB_PASSWORD"),
+        'HOST': env("PRODUCTION_DB_HOST"),
+        'PORT': env("PRODUCTION_DB_PORT"),
+    }
+}
