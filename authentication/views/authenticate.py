dfa1956b327754de16539234837921393f758f36
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-02-16 15:51:01 +0500

    fix: authentication merges issues

--- authentication/views/authenticate.py
+++ authentication/views/authenticate.py
@@ -5,7 +5,6 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from authentication.models import User
 from settings.utils.helpers import get_host
 
