8c396e7b6acc367d88dbc2327e903a76d1c80eb7
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-03-07 00:10:58 +0500

    fix: job_portal classifier logic

--- job_portal/admin.py
+++ job_portal/admin.py
@@ -6,7 +6,8 @@ from job_portal.models import AppliedJobStatus, JobDetail, BlacklistJobs
 # Register your models here.
 
 class JobDetailAdmin(admin.ModelAdmin):
-    list_display = ('job_title', 'company_name', 'job_source', 'job_posted_date', 'job_status')
+    list_display = ('job_title','tech_keywords', 'company_name', 'job_source', 'job_posted_date', 'job_status')
+    list_filter = ('tech_keywords','company_name',)
 
 
 admin.site.register(JobDetail, JobDetailAdmin)
