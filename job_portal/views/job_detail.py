1c623edaac353263e0fe5be4a39e79ff8f2efb56
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-03-24 16:14:44 +0500

    fix: product title search

--- job_portal/views/job_detail.py
+++ job_portal/views/job_detail.py
@@ -22,7 +22,7 @@ class JobDetailsView(ModelViewSet):
     pagination_class = CustomPagination
     filterset_class = CustomJobFilter
     ordering = ('-job_posted_date')
-    search_fields = ['$job_title']
+    search_fields = ['job_title']
     http_method_names = ['get']
     ordering_fields = ['job_title', 'job_type', 'job_posted_date', 'company_name']
     permission_classes = (JobDetailPermission,)
