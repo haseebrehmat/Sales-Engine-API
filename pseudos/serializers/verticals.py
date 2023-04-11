5ad5c7a927a9dd47ec6ec20b099adfbd26ac8358
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-04-11 19:48:58 +0500

    update: pseudo, vertical and scrappers

--- pseudos/serializers/verticals.py
+++ pseudos/serializers/verticals.py
@@ -10,10 +10,8 @@ class VerticalSerializer(serializers.ModelSerializer):
         model = Verticals
         fields = "__all__"
 
-
     def get_hobbies(self, obj):
         queryset = Verticals.objects.filter(id=obj.id).first()
-        print("queryset => ", queryset.hobbies)
         if queryset is not None and queryset.hobbies is not None:
             return queryset.hobbies.split(",")
         else:
