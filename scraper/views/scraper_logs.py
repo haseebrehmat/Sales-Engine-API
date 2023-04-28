c4ffc577474e46d7d41259d2154ca7939f096886
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-04-28 15:23:14 +0500

    fix: scraper logs

--- scraper/views/scraper_logs.py
+++ scraper/views/scraper_logs.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
 
 from scraper.models import ScraperLogs
 from scraper.serializers.scraper_logs_serializer import ScraperLogsSerializer
@@ -8,7 +9,7 @@ from scraper.utils.custom_pagination import CustomPagination
 class ScraperLogView(ListAPIView):
     pagination_class = CustomPagination
     serializer_class = ScraperLogsSerializer
-    # queryset = ScraperLogs.objects.all().order_by('-updated_at')
+    queryset = ScraperLogs.objects.all().order_by('-updated_at')
 
     def get_queryset(self):
         return self.queryset
