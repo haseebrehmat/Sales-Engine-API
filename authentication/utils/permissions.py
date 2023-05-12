4a7461334f80c6a71158f873812c8ec53dcb1826
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-05-12 11:57:54 +0500

    update: permissions

--- authentication/utils/permissions.py
+++ authentication/utils/permissions.py
@@ -294,4 +294,44 @@ permissions = [
         "codename": 'delete_generic_skill',
         "name": 'Delete Generic Skill',
     },
+    {
+        "module": 'Resume Builder',
+        "codename": 'view_resume_builder',
+        "name": 'View Resume Builder',
+    },
+    {
+        "module": 'Status',
+        "codename": 'view_status',
+        "name": 'View All Status',
+    },
+    {
+        "module": 'Status',
+        "codename": 'create_status',
+        "name": 'Create Status',
+    },
+    {
+        "module": 'Status',
+        "codename": 'edit_status',
+        "name": 'Edit Status',
+    },
+    {
+        "module": 'Status',
+        "codename": 'delete_status',
+        "name": 'Delete Status',
+    },
+    {
+        "module": 'Status',
+        "codename": 'view_company_status',
+        "name": 'View Status',
+    },
+    {
+        "module": 'Status',
+        "codename": 'add_company_status',
+        "name": 'Add Status',
+    },
+    {
+        "module": 'Status',
+        "codename": 'remove_company_status',
+        "name": 'Remove Status',
+    }
 ]
