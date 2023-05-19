03ed7f4c5ed0408880a1359fa83582e62c47ac4e
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-05-19 12:15:00 +0500

    Adzuna and simplyhired issues fixed

--- scraper/views/__init__.py
+++ scraper/views/__init__.py
@@ -1,11 +1,14 @@
-import os
-from scraper.models import AllSyncConfig
-from scraper.models import SchedulerSync
-# from scraper.schedulers.job_upload_scheduler import upload_jobs, remove_files
-#
-# upload_jobs()
-# remove_files(job_source="all")
-SchedulerSync.objects.all().update(running=False)
-AllSyncConfig.objects.all().update(status=False)
-if not os.path.exists('scraper/job_data'):
-    os.makedirs('scraper/job_data')
+try:
+    import os
+    from scraper.models import AllSyncConfig
+    from scraper.models import SchedulerSync
+    # from scraper.schedulers.job_upload_scheduler import upload_jobs, remove_files
+    #
+    # upload_jobs()
+    # remove_files(job_source="all")
+    SchedulerSync.objects.all().update(running=False)
+    AllSyncConfig.objects.all().update(status=False)
+    if not os.path.exists('scraper/job_data'):
+        os.makedirs('scraper/job_data')
+except Exception as e:
+    print("Error in job_upload_scheduler init - file", str(e))
