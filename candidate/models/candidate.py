66e1fa5060b204472b7adcd77dfdd2f2934748d0
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-05-29 17:58:33 +0500

    fix: fix issue for developer skills

--- candidate/models/candidate.py
+++ candidate/models/candidate.py
@@ -9,7 +9,7 @@ class Candidate(TimeStamped):
     company = models.ForeignKey(Company, on_delete=models.SET_NULL, blank=True, null=True)
     employee_id = models.CharField(max_length=100, blank=True, null=True)
     name = models.CharField(max_length=100, blank=True, null=True)
-    phone = models.CharField(max_length=12)
+    phone = models.CharField(max_length=30)
     experience = models.CharField(max_length=200, blank=True, null=True)
     email = models.CharField(max_length=100)
     designation = models.ForeignKey(Designation, on_delete=models.SET_NULL, blank=True, null=True)
--- candidate/serializers/candidate.py
+++ candidate/serializers/candidate.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 
-from candidate.models import Candidate, CandidateSkills
-from candidate.serializers.skills import CandidateSkillsSerializer
-
+from candidate.models import Candidate, CandidateSkills, Skills
 
 class CandidateSerializer(serializers.ModelSerializer):
     skills = serializers.SerializerMethodField(default=[])
@@ -14,9 +12,51 @@ class CandidateSerializer(serializers.ModelSerializer):
 
     def get_skills(self, obj):
         queryset = CandidateSkills.objects.filter(candidate=obj)
-        data = []
-        if len(queryset) > 0:
-            serializer_data = CandidateSkillsSerializer(queryset, many=True)
-            data = serializer_data.data
+        data = [x.skill.name for x in queryset]
+
         return data
 
+    def create(self, validated_data):
+        skills = validated_data.pop("skills")
+        temp = []
+        for skill in skills:
+            qs = Skills.objects.filter(name__iexact=skill).first()
+            if qs:
+                temp.append(qs.id)
+            else:
+                qs = Skills.objects.create(name=skill.lower())
+                temp.append(qs.id)
+        skills = temp
+
+        qs = Candidate.objects.create(**validated_data)
+        data = [CandidateSkills(candidate_id=qs.id, skill_id=skill, level=1) for skill in skills]
+        CandidateSkills.objects.bulk_create(data, ignore_conflicts=True)
+
+
+    def update(self, instance, validated_data):
+        skills = validated_data.pop("skills")
+        temp = []
+        for skill in skills:
+            qs = Skills.objects.filter(name__iexact=skill).first()
+            if qs:
+                temp.append(qs.id)
+            else:
+                qs = Skills.objects.create(name=skill.lower())
+                temp.append(qs.id)
+        skills = temp
+
+        instance.employee_id = validated_data.get(
+            "employee_id", instance.employee_id)
+        instance.name = validated_data.get("name", instance.name)
+        instance.phone = validated_data.get("phone", instance.phone)
+        instance.experience = validated_data.get(
+            "experience", instance.experience)
+        instance.email = validated_data.get("email", instance.email)
+        instance.designation = validated_data.get(
+            "designation", instance.designation)
+        instance.save()
+        print(skills)
+        CandidateSkills.objects.filter(candidate_id=instance.id).delete()
+        data = [CandidateSkills(candidate_id=instance.id, skill_id=skill, level=1) for skill in skills]
+        CandidateSkills.objects.bulk_create(data, ignore_conflicts=True)
+        return instance
