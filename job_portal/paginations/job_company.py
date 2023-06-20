97d94847b0e2244a4bbf4fd8bec82813ab1ca255
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-06-20 13:13:38 +0500

    fix: added blocked jobs comapnies stats

--- job_portal/paginations/job_company.py
+++ job_portal/paginations/job_company.py
@@ -1,5 +1,8 @@
+from django.db.models import Q
 from rest_framework import pagination
 from rest_framework.response import Response
+
+from job_portal.models import BlockJobCompany
 
 
 class JobCompanyPagination(pagination.PageNumberPagination):
@@ -8,7 +11,9 @@ class JobCompanyPagination(pagination.PageNumberPagination):
     max_page_size = 500
     page_query_param = 'page'
 
-    def get_paginated_response(self, data):
+    def get_paginated_response(self, data, queryset):
+        total_job_companies = len(queryset) if queryset else 0
+        blocked_job_companies = self.get_blocked_job_companies_count()
         response = Response({
             'links': {
                 'next': self.get_next_link(),
@@ -16,5 +21,23 @@ class JobCompanyPagination(pagination.PageNumberPagination):
                 'num_pages': self.page.paginator.num_pages
             },
             'data': data,
+            'job_companies_stats': {
+                'total': total_job_companies,
+                "blocked": blocked_job_companies,
+                "unblocked": total_job_companies - blocked_job_companies,
+            }
+
         })
         return response
+
+    def get_blocked_job_companies_count(self):
+        search = self.request.query_params['search']
+        search_query = Q()
+        if search:
+            search = search.lower()
+            search_query = Q(company_name__icontains=search)
+        block_job_companies = list(
+            BlockJobCompany.objects.filter(company=self.request.user.profile.company).filter(search_query).values_list(
+                'company_name',
+                flat=True))
+        return len(block_job_companies)
