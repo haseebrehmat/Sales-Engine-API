bb5c6d471205a804ad67b4dc666076cfefaa5608
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-06-20 20:26:46 +0500

    feat: adding new block company migrations

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 from rest_framework import routers
 
 from job_portal.classifier.update_job_stacks import UpdateJobStackView
-# from job_portal.schedulers.job_archive import archive_jobs, scheduler
 from job_portal.views import JobDetailsView, JobDataUploadView, JobCleanerView, ChangeJobStatusView, AppliedJobDetailsView, \
     ListAppliedJobView
 from job_portal.views.blacklist_jobs_source import BlackListJobsView, JobSourcesView, NonBlackListJobsView
@@ -39,7 +38,7 @@ urlpatterns = [
     path('clean_job_source/', JobSourceCleanerView.as_view()),
     path('clean_job_techstacks/', UpdateJobStackView.as_view()),
     path('tech_keywords/', get_tech_keywords),
-    path('delete_duplicated_jobs/', RemoveDuplicateView.as_view())
+    path('delete_duplicated_jobs/', RemoveDuplicateView.as_view()),
     path('all_job_companies/', JobCompaniesList.as_view()),
 ]
 
