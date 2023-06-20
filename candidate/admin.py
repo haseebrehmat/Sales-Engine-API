c9571dfe60869c64830f43bc47da58e213a2339f
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-06-20 17:21:20 +0500

    fix: fix admin py file

--- candidate/admin.py
+++ candidate/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from candidate.models import ExposedCandidate, Candidate, Designation, Skills, CandidateSkills, Tools, CandidateTools,\
-    CandidateProjects, ProjectImage
+    CandidateProjects, ProjectImage, Regions, CandidateRegions
 from candidate.models.candidate_company import CandidateCompany
 
 admin.site.register(CandidateCompany)
 # Register your models here.
 admin.site.register([ExposedCandidate, Candidate, Designation, Skills, CandidateSkills, Tools, CandidateTools,
-                     CandidateProjects, ProjectImage])
+                     CandidateProjects, ProjectImage, Regions, CandidateRegions])
