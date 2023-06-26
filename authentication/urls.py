5e15a8a81430af4ee0ad7b13d0c7ab0a2b6e06de
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-06-26 19:35:29 +0500

    feat: Implement permission cruds for permission module

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-
 from authentication.views.create_permissions import CreatePermissions
 from authentication.views.authenticate import UserLogin
 from authentication.views.company import CompanyView, CompanyDetailView
--- authentication/views/permission.py
+++ authentication/views/permission.py
@@ -7,44 +7,42 @@ from authentication.exceptions import InvalidUserException
 from authentication.models import CustomPermission
 from authentication.permissions import RolePermissions
 from authentication.serializers.permissions import PermissionSerializer
+from rest_framework.generics import ListAPIView
 
 
 from settings.utils.helpers import serializer_errors
 
 
-class PermissionView(APIView):
-    permission_classes = (RolePermissions,)
+class PermissionView(ListAPIView):
+    serializer_class = PermissionSerializer
 
-    def get(self, request):
-        queryset = CustomPermission.objects.all()
-        if request.GET.get("module", "") != "":
-            queryset = queryset.filter(module__iexact=request.GET.get("module"))
-        serializer = PermissionSerializer(queryset, many=True)
-        data = serializer.data
-        if len(data) > 0:
-            modules = set([x["module"] for x in data])
-            temp = []
-            for x in modules:
-                temp.append({"module": x, "permissions": [i for i in data if i["module"] == x]})
-            data = temp
-        else:
-            data = []
-        return Response(data, status=status.HTTP_200_OK)
+    def get_queryset(self):
+        return CustomPermission.objects.all()
+
 
     def post(self, request):
-        serializer = PermissionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.create(serializer.validated_data)
-            message = "Permission created successfully!"
-            status_code = status.HTTP_201_CREATED
-            return Response({'detail': message}, status=status_code)
-        data = serializer_errors(serializer)
-        raise InvalidUserException(data)
+        permissions = request.data["permissions"]
+        for permission in permissions:
+            serializer = PermissionSerializer(data=permission)
+            if serializer.is_valid():
+                print("Entered in if")
+                continue
+            else:
+                print("Entered in else")
+                data = serializer_errors(serializer)
+                if data == "non_field_errors: The fields module, codename, name must make a unique set." :
+                    codename = permission["codename"]
+                    module = permission["module"]
+
+                    data = f"{codename} permission already exist in {module} module"
+                raise InvalidUserException(data)
+        serializer.create(request.data)
+        message = "Permission created successfully!"
+        status_code = status.HTTP_201_CREATED
+        return Response({'detail': message}, status=status_code)
 
 
 class PermissionDetailView(APIView):
-    permission_classes = (RolePermissions,)
-
     def get(self, request, pk):
         queryset = CustomPermission.objects.filter(pk=pk).first()
         serializer = PermissionSerializer(queryset, many=False)
@@ -57,11 +55,12 @@ class PermissionDetailView(APIView):
 
         if serializer.is_valid():
             serializer.save()
-
             message = "Permission updated successfully!"
             status_code = status.HTTP_200_OK
             return Response({'detail': message}, status=status_code)
         data = serializer_errors(serializer)
+        if data == "non_field_errors: The fields module, codename, name must make a unique set.":
+            data = "Permission already exist"
         raise InvalidUserException(data)
 
     def delete(self, request, pk):
