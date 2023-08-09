b929562c8a658747e3958d5dfc14b5b5c004fc3e
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-08-09 13:02:07 +0500

    fix: add comments

--- candidate/views/exposed_team.py
+++ candidate/views/exposed_team.py
@@ -58,7 +58,7 @@ class ExposedTeamListAPIView(APIView):
 
     # Yahan ab team ko unexpose krna h or team m ak feild add h expose_to us m company ids available hn jin k liy team expose hoti h wahan s company ids to
     # delete krty jana h jo jo unexpose hn gi or front end s hamary pass jo team id ay gi us k sary exposed candidates nikal k loop laga k unexposed candidate wala kam repeat ho ga
-    # or team m s exposed candidate ki logic upar waly function m implemented h 
+    # or team m s exposed candidate ki logic upar waly function m implemented h
 
 
 
