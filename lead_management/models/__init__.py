a0ebf188d99096e5e5052d30238d0b938976b6b6
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-08-22 19:49:45 +0500

    fix: added file attachments for notes on lead management

--- lead_management/models/__init__.py
+++ lead_management/models/__init__.py
@@ -5,3 +5,4 @@ from .lead_activity import LeadActivity
 from .lead_activity_notes import LeadActivityNotes
 from .phase import Phase
 from .status import Status
+from .lead_activity_notes_attachments import LeadActivityNotesAttachment
