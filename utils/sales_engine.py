0beb9ded94f38666407b3317c352b0ad24b35f98
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-09-01 15:52:41 +0500

    fix: sales engine url issue

--- utils/sales_engine.py
+++ utils/sales_engine.py
@@ -13,37 +13,37 @@ from utils.requests_logger import requests_logger_hooks
 @start_new_thread
 def upload_jobs_in_sales_engine(jobs_data, filename=None):
     try:
-        # url = SALES_ENGINE_UPLOAD_JOBS_URL
-        #
-        # payload = json.dumps({
-        #     "jobs": [
-        #         {
-        #             "job_title": job.job_title,
-        #             "job_source_url": job.job_source_url,
-        #             "job_type": job.job_type,
-        #             "job_posted_date": job.job_posted_date.strftime('%Y-%m-%d'),
-        #             "job_source": job.job_source,
-        #             "job_description": job.job_description,
-        #             "company_name": job.company_name,
-        #             "address": job.address
-        #         } for job in jobs_data]
-        # }
-        # )
-        #
-        # headers = {
-        #     'Authorization': SALES_ENGINE_API_TOKEN,
-        #     'Content-Type': 'application/json'}
-        #
-        # response = requests.request("POST", url, headers=headers, data=payload, hooks=requests_logger_hooks)
-        #
-        # # print(response.text)
-        # if response.ok:
-        #     if filename:
-        #         job_source = filename.replace('scraper/job_data/', '').split(' ')[0]
-        #     else:
-        #         if jobs_data:
-        #             job_source = jobs_data[0].job_source
-        #     obj = SalesEngineJobsStats.objects.create(job_source=job_source, jobs_count=len(jobs_data))
+        url = SALES_ENGINE_UPLOAD_JOBS_URL
+
+        payload = json.dumps({
+            "jobs": [
+                {
+                    "job_title": job.job_title,
+                    "job_source_url": job.job_source_url,
+                    "job_type": job.job_type,
+                    "job_posted_date": job.job_posted_date.strftime('%Y-%m-%d'),
+                    "job_source": job.job_source,
+                    "job_description": job.job_description,
+                    "company_name": job.company_name,
+                    "address": job.address
+                } for job in jobs_data]
+        }
+        )
+
+        headers = {
+            'Authorization': SALES_ENGINE_API_TOKEN,
+            'Content-Type': 'application/json'}
+
+        response = requests.request("POST", url, headers=headers, data=payload, hooks=requests_logger_hooks)
+
+        # print(response.text)
+        if response.ok:
+            if filename:
+                job_source = filename.replace('scraper/job_data/', '').split(' ')[0]
+            else:
+                if jobs_data:
+                    job_source = jobs_data[0].job_source
+            obj = SalesEngineJobsStats.objects.create(job_source=job_source, jobs_count=len(jobs_data))
 
         upload_jobs_in_sales_engine_staging(jobs_data, filename=None)
     except Exception as e:
@@ -62,7 +62,7 @@ def upload_jobs_in_sales_engine_staging(jobs_data, filename=None):
         job_roles = json.loads(resp.text).get('job_roles', []) if resp.ok else []
 
         excluded_jobs = ['others', 'others dev', 'other', 'other dev']
-        url = 'https://decagon-staging.devsinc.com/job_portal/api/v1/job_roles'
+        url = 'https://decagon-staging.devsinc.com/job_portal/api/v1/jobs'
 
         payload = json.dumps({
             "jobs": [
@@ -97,19 +97,6 @@ def upload_jobs_in_sales_engine_staging(jobs_data, filename=None):
     except Exception as e:
         saveLogs(e)
 
-
-headers = {
-    'Authorization': '445f188bsk3423dsd1342jj434hjkn43j43n43j4d875ee0995ac1e89de6fc1d0252aabc5f2b24a4928',
-    'content-type': 'application/json'
-}
-resp = requests.get('https://decagon-staging.devsinc.com/job_portal/api/v1/job_roles', headers=headers)
-
-
-# keywords_dict = {k.lower().replace(" ", ""): v for k, v in keyword.items()}
-# print(keywords_dict)
-# keywords = [x.lower() for x in set(JobDetail.objects.values_list('tech_keywords', flat=True))]
-
-# print(keywords)
 
 def check_job_role(tech, job_roles):
     regular_expression = [item for item in regular_expressions if item['tech_stack'] == tech]
