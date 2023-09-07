3c44a81d1e85d010bbfad3b9ce62cb640a01e5fc
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-09-07 20:16:09 +0500

    feat: implement hirenovice scraper on backend

--- scraper/views/sync_scheduler.py
+++ scraper/views/sync_scheduler.py
@@ -43,6 +43,7 @@ def run_scrapers_manually(job_source='all'):
             "builtin",
             "workable",
             "themuse",
+            "hirenovice",
     ]
     if job_source.lower() not in valid_job_sources:
         return {"detail": f"{job_source} not a valid job source"}, status.HTTP_406_NOT_ACCEPTABLE
