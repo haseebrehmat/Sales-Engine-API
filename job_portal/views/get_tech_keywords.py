61acfb0a920d209244a2a11718ab567ed7ed3f05
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-11-06 19:01:26 +0500

    fix: missing exluded keywords issue & add TechStats in these keywords

--- job_portal/views/get_tech_keywords.py
+++ job_portal/views/get_tech_keywords.py
@@ -2,14 +2,19 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from job_portal.models import JobDetail
+from job_portal.models import JobDetail, JobArchive, TechStats
 
 
 @api_view(['GET'])
 #@permission_classes((AllowAny, ))
 def get_tech_keywords(request):
-    keywords = JobDetail.objects.exclude(tech_stacks=None).values_list("tech_stacks", flat=True)
+    keywords_from_detail = JobDetail.objects.exclude(tech_stacks=None).values_list("tech_stacks", flat=True)
+    keywords_from_archive = JobArchive.objects.exclude(tech_keywords=None).values_list("tech_keywords", flat=True)
+    keywords_from_stats = TechStats.objects.exclude(name=None).values_list("name", flat=True)
+    keywords = [x.split(",") for x in keywords_from_archive]
+    keywords.extend(list(keywords_from_detail))
     data = []
     for x in keywords:
         data.extend(x)
+    data.extend(list(keywords_from_stats))
     return Response({"keywords": list(set(data))})
