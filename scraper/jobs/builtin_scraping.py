60e51ab18285490019e059ac9d60ad63469f6908
haseebrehmat <haseeb.ali.dev@gmail.com>
2023-11-23 17:32:21 +0500

    update: remove extra imports

--- scraper/jobs/builtin_scraping.py
+++ scraper/jobs/builtin_scraping.py
@@ -1,13 +1,6 @@
 import time
-from datetime import datetime
 
 import pandas as pd
-from regex import D, F
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
 from scraper.constants.const import *
