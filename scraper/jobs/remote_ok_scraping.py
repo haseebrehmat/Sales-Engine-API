42375f8c751a5504dfcce59aced1b62f60a4bbb8
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-05-16 20:22:31 +0500

    fix: analytics issue fixed

--- scraper/jobs/remote_ok_scraping.py
+++ scraper/jobs/remote_ok_scraping.py
@@ -97,83 +97,86 @@ def find_jobs(driver, job_type):
     links = get_job_urls(driver)
     total_job = len(links)
     original_window = driver.current_window_handle
-    for link in links:
-        data = []
-        if link:
-            try:
-                driver.switch_to.new_window('tab')
-                driver.get("https://remoteok.com" + link)
-                time.sleep(5)
-                id = link.split("-")[-1]
-                if is_convertible_to_number(id):
-                    id = int(id)
-                    job = WebDriverWait(driver, 40).until(
-                        EC.presence_of_element_located(
-                            (By.CLASS_NAME, f"job-{id}")))
-                    job_desc = WebDriverWait(driver, 40).until(
-                        EC.presence_of_element_located(
-                            (By.CLASS_NAME, "expandContents")))
+    try:
+        for link in links:
+            data = []
+            if link:
+                try:
+                    driver.switch_to.new_window('tab')
+                    driver.get("https://remoteok.com" + link)
+                    time.sleep(5)
+                    id = link.split("-")[-1]
+                    if is_convertible_to_number(id):
+                        id = int(id)
+                        job = WebDriverWait(driver, 40).until(
+                            EC.presence_of_element_located(
+                                (By.CLASS_NAME, f"job-{id}")))
+                        job_desc = WebDriverWait(driver, 40).until(
+                            EC.presence_of_element_located(
+                                (By.CLASS_NAME, "expandContents")))
 
-                    temp = job.find_element(
-                        By.CLASS_NAME, "company_and_position").text
-                    temp = temp.splitlines()
-                    job_title = remove_emojis(temp[0])
-                    append_data(data, job_title)
+                        temp = job.find_element(
+                            By.CLASS_NAME, "company_and_position").text
+                        temp = temp.splitlines()
+                        job_title = remove_emojis(temp[0])
+                        append_data(data, job_title)
 
-                    company_name = remove_emojis(temp[1])
-                    append_data(data, company_name)
+                        company_name = remove_emojis(temp[1])
+                        append_data(data, company_name)
 
-                    address = 'Remote'
-                    append_data(data, address)
+                        address = 'Remote'
+                        append_data(data, address)
 
-                    job_source_url = link
-                    append_data(data, job_source_url)
+                        job_source_url = link
+                        append_data(data, job_source_url)
 
-                    job_description = job_desc.text
-                    append_data(data, job_description)
+                        job_description = job_desc.text
+                        append_data(data, job_description)
 
-                    temp1 = job.find_element(By.CLASS_NAME, "time").text
-                    job_posted_date = temp1
-                    append_data(data, job_posted_date)
+                        temp1 = job.find_element(By.CLASS_NAME, "time").text
+                        job_posted_date = temp1
+                        append_data(data, job_posted_date)
 
-                    salary_format = "yearly"
-                    append_data(data, salary_format)
+                        salary_format = "yearly"
+                        append_data(data, salary_format)
 
-                    temp2 = temp[-1].split(" ")
-                    if len(temp2[-3]) >= 4 and temp2[-3][0] == "$":
-                        salary_min = remove_emojis(temp2[-3])
-                    else:
-                        salary_min = "N/A"
-                    append_data(data, salary_min)
+                        temp2 = temp[-1].split(" ")
+                        if len(temp2[-3]) >= 4 and temp2[-3][0] == "$":
+                            salary_min = remove_emojis(temp2[-3])
+                        else:
+                            salary_min = "N/A"
+                        append_data(data, salary_min)
 
-                    if len(temp2[-1]) >= 4 and temp2[-1][0] == "$":
-                        salary_max = remove_emojis(temp2[-1])
-                    else:
-                        salary_max = "N/A"
-                    append_data(data, salary_max)
+                        if len(temp2[-1]) >= 4 and temp2[-1][0] == "$":
+                            salary_max = remove_emojis(temp2[-1])
+                        else:
+                            salary_max = "N/A"
+                        append_data(data, salary_max)
 
-                    if salary_max == "N/A" or salary_min == "N/A":
-                        estimated_salary = "N/A"
-                    else:
-                        estimated_salary = f"{salary_min}-{salary_max}"
-                    append_data(data, estimated_salary)
+                        if salary_max == "N/A" or salary_min == "N/A":
+                            estimated_salary = "N/A"
+                        else:
+                            estimated_salary = f"{salary_min}-{salary_max}"
+                        append_data(data, estimated_salary)
 
-                    job_source = ScraperNaming.REMOTE_OK
-                    append_data(data, job_source)
+                        job_source = ScraperNaming.REMOTE_OK
+                        append_data(data, job_source)
 
-                    job_type = "full time"
-                    sub_type = "remote"
-                    append_data(data, set_job_type(job_type, sub_type))
+                        job_type = "full time"
+                        sub_type = "remote"
+                        append_data(data, set_job_type(job_type, sub_type))
 
-                    job_description_tags = job_desc.get_attribute("innerHTML")
-                    append_data(data, str(job_description_tags))
+                        job_description_tags = job_desc.get_attribute("innerHTML")
+                        append_data(data, str(job_description_tags))
 
-            except exception as e:
-                print(e)
-            
-            driver.close()
-            driver.switch_to.window(original_window)
-            scrapped_data.append(data)
+                except exception as e:
+                    print(e)
+                
+                driver.close()
+                driver.switch_to.window(original_window)
+                scrapped_data.append(data)
+    except Exception as e:
+        print(e)
 
     columns_name = [
         "job_title",
