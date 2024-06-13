d76ef8f9c20cb808ebaac42c90093bbe0ec6d2ef
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-06-13 17:55:38 +0500

    MERGED :: OCTA-30 :: Scraper :: Fix and optimize dice scraper (#673)
    * dice scraper fixed and optimized
    
    * log running time
    
    * remove total jobs
    
    * constants and add line
    
    * set to conditional check
    
    ---------
    
    Co-authored-by: uzairmin <uzair.am10@gmail.com>

--- scraper/constants/const.py
+++ scraper/constants/const.py
@@ -56,3 +56,5 @@ ERROR_TEXT = "Error occured while scraping"
 # JOB_TYPE = ["Full Time Remote", "Contract Remote"]
 JOB_TYPE = ["Full Time Remote", "Full Time on Site", "Full Time Hybrid", "Contract Hybrid", "Contract Onsite", "Contract Remote"]
 SCRAPERS_NAME = ['weworkremotely', 'glassdoor', 'indeed', 'linkedin', 'rubyonremote', 'justremote', 'monster', 'remoteok', 'builtin', 'workable', 'ziprecruiter', 'simplyhired', 'ycombinator', 'dice', 'talent']
+COLUMN_NAME = ["job_title", "company_name", "address", "job_description", 'job_source_url', "job_posted_date", "salary_format", "estimated_salary", "salary_min", "salary_max", "job_source", "job_type", "job_description_tags"]
+REMOTE = 'remote'
--- scraper/jobs/dice_scraping.py
+++ scraper/jobs/dice_scraping.py
@@ -11,11 +11,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from scraper.constants.const import *
 from scraper.models.scraper_logs import ScraperLogs
-from scraper.utils.helpers import generate_scraper_filename, ScraperNaming, configure_webdriver, set_job_type
-from utils.helpers import saveLogs
-
-total_job = 0
-
+from scraper.utils.helpers import generate_scraper_filename, ScraperNaming, configure_webdriver, previous_jobs, set_job_type
+from utils.helpers import log_scraper_running_time, saveLogs
 
 # calls url
 def request_url(driver, url):
@@ -28,81 +25,69 @@ def append_data(data, field):
 
 
 # find's job name
-def find_jobs(driver, job_type, total_job):
-    scrapped_data = []
-    count = -1
-    WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "card-title-link"))
-    )
-    jobs = driver.find_elements(By.TAG_NAME, "dhi-search-card")
-
-    job_title = driver.find_elements(By.CLASS_NAME, "card-title-link")
-    original_window = driver.current_window_handle
-    for job in jobs:
-        driver.switch_to.window(original_window)
-        count += 1
-        try:
-            data = []
-            append_data(data, job_title[count].text)
-            c_name = driver.find_elements(By.CLASS_NAME, "card-company")
-            company_name = c_name[count].find_element(By.TAG_NAME, "a")
-            append_data(data, company_name.text)
-            address = driver.find_elements(By.CLASS_NAME, "search-result-location")
-            append_data(data, address[count].text)
-            # original_window = driver.current_window_handle
-            job_title[count].click()
-            time.sleep(2)
-            driver.switch_to.window(driver.window_handles[-1])
-            job_url = driver.current_url
-            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "job-description")))
-            try:
-                driver.find_element(By.ID, "descriptionToggle").click()
-            except:
-                driver.close()
-                continue
-            job_description = driver.find_element(By.CLASS_NAME, "job-description")
-            append_data(data, job_description.text)
-            append_data(data, job_url)
-            job_posted_date = driver.find_element(By.CLASS_NAME, "sc-dhi-time-ago")
-            append_data(data, job_posted_date.text)
-            append_data(data, "N/A")
-            append_data(data, "N/A")
-            append_data(data, "N/A")
-            append_data(data, "N/A")
-            append_data(data, "Dice")
-            append_data(data, set_job_type(job_type, determine_job_sub_type(job_type)))
-            append_data(data, job_description.get_attribute('innerHTML'))
-
-            total_job += 1
-            scrapped_data.append(data)
-            driver.close()
-            # driver.switch_to.window(original_window)
-            time.sleep(0.5)
-        except Exception as e:
-            print(e)
-        time.sleep(1)
-
-    columns_name = ["job_title", "company_name", "address", "job_description", 'job_source_url', "job_posted_date", "salary_format",
-                    "estimated_salary", "salary_min", "salary_max", "job_source", "job_type", "job_description_tags"]
-    df = pd.DataFrame(data=scrapped_data, columns=columns_name)
-    filename = generate_scraper_filename(ScraperNaming.DICE)
-    df.to_excel(filename, index=False)
-
-    ScraperLogs.objects.create(total_jobs=len(df), job_source="Dice", filename=filename)
-    driver.switch_to.window(original_window)
-    finished = "disabled"
-    pagination = driver.find_elements(By.CLASS_NAME, "pagination-next")
+def find_jobs(driver, job_type, job_urls):
     try:
-        next_page = pagination[0].get_attribute('class')
-        if finished in next_page:
-            return False, total_job
-        else:
-            pagination[0].click()
-            time.sleep(5)
-        return True, total_job
-    except Exception as e:
-        print(e)
-        return False, total_job
+        scrapped_data = []
+
+        existing_jobs_dictionary = previous_jobs("dice", job_urls)
+
+        original_window = driver.current_window_handle
+        driver.switch_to.new_window('tab')
+        for url in job_urls:
+            try:
+                if not existing_jobs_dictionary.get(url):
+                    data = []
+                    driver.get(url)
+                    job_title = driver.find_element(By.TAG_NAME, "h1")
+                    append_data(data, job_title.text)
+                    company_name = driver.find_element(By.CLASS_NAME, "companyInfo")
+                    append_data(data, company_name.text)
+                    address = driver.find_elements(By.CLASS_NAME, "job-header_jobDetail__ZGjiQ")
+                    job_location = REMOTE
+                    if 'Posted' in address[0].text:
+                        job_posted_date = address[0].text.split(' |')[0]
+                    else:
+                        try:
+                            job_location = address[0].text
+                            job_posted_date = address[1].text.split(' |')[0]
+                        except:
+                            continue
+                    append_data(data, job_location)
+
+                    time.sleep(2)
+                    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "job-description")))
+                    try:
+                        driver.find_element(By.ID, "descriptionToggle").click()
+                    except:
+                        driver.close()
+                        continue
+                    job_description = driver.find_element(By.CLASS_NAME, "job-description")
+                    append_data(data, job_description.text)
+                    append_data(data, url)
+                    append_data(data, job_posted_date)
+                    append_data(data, "N/A")
+                    append_data(data, "N/A")
+                    append_data(data, "N/A")
+                    append_data(data, "N/A")
+                    append_data(data, "Dice")
+                    append_data(data, set_job_type(job_type, determine_job_sub_type(job_type)))
+                    append_data(data, job_description.get_attribute('innerHTML'))
+
+                    scrapped_data.append(data)
+                    time.sleep(0.5)
+            except:
+                pass
+
+        driver.close()
+        driver.switch_to.window(original_window)
+
+        df = pd.DataFrame(data=scrapped_data, columns=COLUMN_NAME)
+        filename = generate_scraper_filename(ScraperNaming.DICE)
+        df.to_excel(filename, index=False)
+
+        ScraperLogs.objects.create(total_jobs=len(df), job_source="Dice", filename=filename)
+    except:
+        pass
     
 def determine_job_sub_type(type):
     sub_type = 'remote'
@@ -111,28 +96,50 @@ def determine_job_sub_type(type):
     if 'hybrid' in type.strip().lower():
         sub_type = 'hybrid'
     return sub_type
-    
+
+def load_jobs(driver, job_urls):
+    try:
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "card-title-link"))
+        )
+
+        jobs = driver.find_elements(By.TAG_NAME, "dhi-search-card")
+        job_urls += [f"https://www.dice.com/job-detail/{job.find_element(By.CLASS_NAME, 'card-title-link').get_attribute('id')}" for job in jobs if job.find_element(By.CLASS_NAME, 'card-title-link')]
+
+        pagination = driver.find_elements(By.CLASS_NAME, "pagination-next")
+        try:
+            next_page = pagination[0].get_attribute('class')
+            if 'disabled' in next_page:
+                return False, job_urls
+            else:
+                pagination[0].click()
+                time.sleep(3)
+            return True, job_urls
+        except Exception as e:
+            return False, job_urls
+    except:
+        return False, job_urls
 
 
 # code starts from here
+@log_scraper_running_time("dice")
 def dice(link, job_type):
-    total_job = 0
     print("Dice")
     driver = configure_webdriver()
     try:
         driver.maximize_window()
+        job_urls = []
         flag = True
         try:
             request_url(driver, link)
             while flag:
-                flag, total_job = find_jobs(driver, job_type, total_job)
-                print("Fetching...")
-            print(SCRAPING_ENDED)
+                flag, job_urls = load_jobs(driver, job_urls)
+            find_jobs(driver, job_type, job_urls)
         except Exception as e:
             saveLogs(e)
-            print(LINK_ISSUE)
 
     except Exception as e:
         saveLogs(e)
-        print(e)
-    driver.quit()
+    finally:
+        driver.quit()
+    
