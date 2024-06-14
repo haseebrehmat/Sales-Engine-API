755bf0ee3c527b5df93e4989b467bddd29f8e5f6
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-06-14 18:07:23 +0500

    MERGED :: OCTA-22 :: Scraper :: Fix and Optimize RemoteOK (#664)
    * fix and optimize remoteok
    
    * add log decorator
    
    * modify filtering mechanism
    
    * used COLUMN_NAME constant

--- scraper/jobs/remote_ok_scraping.py
+++ scraper/jobs/remote_ok_scraping.py
@@ -1,38 +1,30 @@
 from datetime import timedelta, datetime
-from django.utils import timezone
 from logging import exception
-import sys
-import traceback
 import pandas as pd
-import time
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 
 from scraper.constants.const import *
 from scraper.models.scraper_logs import ScraperLogs
-from scraper.utils.helpers import generate_scraper_filename, ScraperNaming, remove_emojis, k_conversion, configure_webdriver, set_job_type
-from utils.helpers import saveLogs
+from scraper.utils.helpers import generate_scraper_filename, ScraperNaming, remove_emojis, k_conversion, configure_webdriver, set_job_type, previous_jobs, sleeper
+from utils.helpers import saveLogs, log_scraper_running_time
 
 
 def load_jobs(driver):
-    previous_len = len(driver.find_elements(
-        By.CLASS_NAME, "company_and_position"))
-    count = 0
+    previous_jobs = driver.find_elements(By.CLASS_NAME, "job")
+    previous_len = len(previous_jobs)
+
     while True:
-        if count == 5:
+        if not check_if_job_day_ago(previous_jobs[previous_len-1]):
             break
-        count += 1
-        time.sleep(3)
+        sleeper()
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-        time.sleep(5)
-        elements = driver.find_elements(By.CLASS_NAME, "company_and_position")
-        if previous_len == len(elements):
+        sleeper()
+        previous_jobs = driver.find_elements(By.CLASS_NAME, "job")
+        if previous_len == len(previous_jobs):
             break
-        previous_len = len(elements)
+        previous_len = len(previous_jobs)
 
 
 def get_job_urls(driver):
@@ -43,31 +35,30 @@ def get_job_urls(driver):
                 (By.CSS_SELECTOR, "tr[data-slug][data-id]"))
         )
     except Exception as error:
-        print(error)
+        saveLogs(e)
     job_urls = driver.find_elements(By.CSS_SELECTOR, "tr[data-slug][data-id]")
     links = []
     for job_url in job_urls:
         try:
-            if (check_if_job_week_ago(job_url)):
-                link = job_url.get_attribute("data-href")
-                links.append(link)
+            link = job_url.get_attribute("data-href")
+            links.append("https://remoteok.com"+link)
         except Exception as e:
-            print(e)
+            saveLogs(e)
     return links
 
-def check_if_job_week_ago(job):
-    week_ago = False
+def check_if_job_day_ago(job):
+    day_ago = False
     try:
         date_elm = job.find_element(By.TAG_NAME, "time")
         date_str = date_elm.get_attribute("datetime")
-        date_obj = datetime.fromisoformat(date_str).replace(tzinfo=timezone.get_current_timezone())
-        current_date = datetime.now(tz=timezone.get_current_timezone())
-        one_week_ago_date = current_date - timedelta(days=7)
-        if date_obj >= one_week_ago_date:
-            week_ago = True
+        date_obj = datetime.fromisoformat(date_str)
+        current_date = datetime.now(tz=date_obj.tzinfo)
+        one_day_ago_date = current_date - timedelta(days=1)
+        if date_obj >= one_day_ago_date: 
+            day_ago = True
     except Exception as e:
-        print(e)
-    return week_ago
+        saveLogs(e)
+    return day_ago
 
 # calls url
 
@@ -93,136 +84,101 @@ def append_data(data, field):
 
 def find_jobs(driver, job_type):
     scrapped_data = []
-
     links = get_job_urls(driver)
-    total_job = len(links)
+    existing_jobs = previous_jobs(source='remoteok',urls=links)
     original_window = driver.current_window_handle
     try:
         for link in links:
             data = []
-            if link:
-                try:
-                    driver.switch_to.new_window('tab')
-                    driver.get("https://remoteok.com" + link)
-                    time.sleep(5)
-                    id = link.split("-")[-1]
-                    if is_convertible_to_number(id):
-                        id = int(id)
-                        job = WebDriverWait(driver, 40).until(
-                            EC.presence_of_element_located(
-                                (By.CLASS_NAME, f"job-{id}")))
-                        job_desc = WebDriverWait(driver, 40).until(
-                            EC.presence_of_element_located(
-                                (By.CLASS_NAME, "expandContents")))
+            if existing_jobs.get(link):
+                continue
+            try:
+                driver.switch_to.new_window('tab')
+                driver.get(link)
+                sleeper()
+                id = link.split("-")[-1]
+                if is_convertible_to_number(id):
+                    id = int(id)
+                    job = WebDriverWait(driver, 40).until(
+                        EC.presence_of_element_located(
+                        (By.CLASS_NAME, f"job-{id}")))
+                    job_desc = WebDriverWait(driver, 40).until(
+                        EC.presence_of_element_located(
+                        (By.CLASS_NAME, "expandContents")))
 
-                        temp = job.find_element(
-                            By.CLASS_NAME, "company_and_position").text
-                        temp = temp.splitlines()
-                        job_title = remove_emojis(temp[0])
-                        append_data(data, job_title)
-
-                        company_name = remove_emojis(temp[1])
-                        append_data(data, company_name)
-
-                        address = 'Remote'
-                        append_data(data, address)
-
-                        job_source_url = link
-                        append_data(data, job_source_url)
-
-                        job_description = job_desc.text
-                        append_data(data, job_description)
-
-                        temp1 = job.find_element(By.CLASS_NAME, "time").text
-                        job_posted_date = temp1
-                        append_data(data, job_posted_date)
-
-                        salary_format = "yearly"
-                        append_data(data, salary_format)
-
-                        temp2 = temp[-1].split(" ")
-                        if len(temp2[-3]) >= 4 and temp2[-3][0] == "$":
-                            salary_min = remove_emojis(temp2[-3])
-                        else:
-                            salary_min = "N/A"
-                        append_data(data, salary_min)
-
-                        if len(temp2[-1]) >= 4 and temp2[-1][0] == "$":
-                            salary_max = remove_emojis(temp2[-1])
-                        else:
-                            salary_max = "N/A"
-                        append_data(data, salary_max)
-
-                        if salary_max == "N/A" or salary_min == "N/A":
-                            estimated_salary = "N/A"
-                        else:
-                            estimated_salary = f"{salary_min}-{salary_max}"
-                        append_data(data, estimated_salary)
-
-                        job_source = ScraperNaming.REMOTE_OK
-                        append_data(data, job_source)
-
-                        job_type = "full time"
-                        sub_type = "remote"
-                        append_data(data, set_job_type(job_type, sub_type))
-
-                        job_description_tags = job_desc.get_attribute("innerHTML")
-                        append_data(data, str(job_description_tags))
-
-                except exception as e:
-                    print(e)
+                    temp = job.find_element(
+                        By.CLASS_NAME, "company_and_position").text
+                    temp = temp.splitlines()
+                    job_title = remove_emojis(temp[0])
+                    append_data(data, job_title)
+                    company_name = remove_emojis(temp[1])
+                    append_data(data, company_name)
+                    address = remove_emojis(temp[2])
+                    append_data(data, address)
+                    job_source_url = link
+                    append_data(data, job_source_url)
+                    job_description = job_desc.text
+                    append_data(data, job_description)
+                    temp1 = job.find_element(By.CLASS_NAME, "time").text
+                    job_posted_date = temp1
+                    append_data(data, job_posted_date)
+                    salary_format = "yearly"
+                    append_data(data, salary_format)
+                    temp2 = temp[-1].split(" ")
+                    if len(temp2[-3]) >= 4 and temp2[-3][0] == "$":
+                        salary_min = remove_emojis(temp2[-3])
+                    else:
+                        salary_min = "N/A"
+                    append_data(data, salary_min)
+                    if len(temp2[-1]) >= 4 and temp2[-1][0] == "$":
+                        salary_max = remove_emojis(temp2[-1])
+                    else:
+                        salary_max = "N/A"
+                    append_data(data, salary_max)
+                    if salary_max == "N/A" or salary_min == "N/A":
+                        estimated_salary = "N/A"
+                    else:
+                        estimated_salary = f"{salary_min}-{salary_max}"
+                    append_data(data, estimated_salary)
+                    job_source = ScraperNaming.REMOTE_OK
+                    append_data(data, job_source)
+                    append_data(data, job_type)
+                    job_description_tags = job_desc.get_attribute("innerHTML")
+                    append_data(data, str(job_description_tags))
+            except exception as e:
+                    saveLogs(e)
                 
-                driver.close()
-                driver.switch_to.window(original_window)
-                scrapped_data.append(data)
+            driver.close()
+            driver.switch_to.window(original_window)
+            if data == []:
+                continue
+            scrapped_data.append(data)
     except Exception as e:
-        print(e)
+        saveLogs(e)
+    if len(scrapped_data) > 0:
+        export_to_excel(scrapped_data)
 
-    columns_name = [
-        "job_title",
-        "company_name",
-        "address",
-        "job_source_url",
-        "job_description",
-        "job_posted_date",
-        "salary_format",
-        "salary_min",
-        "salary_max",
-        "estimated_salary",
-        "job_source",
-        "job_type",
-        "job_description_tags",
-    ]
-
-    df = pd.DataFrame(data=scrapped_data, columns=columns_name)
+def export_to_excel(scrapped_data):
+    df = pd.DataFrame(data=scrapped_data, columns=COLUMN_NAME)
     filename = generate_scraper_filename(ScraperNaming.REMOTE_OK)
     df.to_excel(filename, index=False)
 
     ScraperLogs.objects.create(
         total_jobs=len(df), job_source="Remote ok", filename=filename
     )
-    return False, total_job
 
 
 # code starts from here
+@log_scraper_running_time("RemoteOk")
 def remoteok(link, job_type):
-    print("Remote Ok")
     driver = configure_webdriver()
     try:
         driver.maximize_window()
-        flag = True
-        try:
-            request_url(driver, link)
-            while flag:
-                flag, _ = find_jobs(driver, job_type)
-                print("Fetching...")
-            print(SCRAPING_ENDED)
-
-        except Exception as e:
-            saveLogs(e)
-            print(LINK_ISSUE)
-
+        sleeper()
+        request_url(driver, link)
+        sleeper()
+        find_jobs(driver, job_type)
     except Exception as e:
         saveLogs(e)
-        print(e)
-    driver.quit()
+    finally:
+        driver.quit()
