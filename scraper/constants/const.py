442aab1dbdd40521a2bbfd08bec3c442029cdcea
haseebrehmat <haseeb.ali.dev@gmail.com>
2024-07-04 21:01:46 +0500

    MERGE :: OCTA-51 :: Scraper :: Base Scraper class (#687)
    * Base Scraper class
    
    * Base Scraper class
    
    * optimize base scraper class
    
    * optimize base scraper class
    
    * test base scraper with just remote
    
    * remove .xlsx file
    
    * add destructor in base scraper

--- scraper/constants/const.py
+++ scraper/constants/const.py
@@ -59,3 +59,14 @@ SCRAPERS_NAME = ['weworkremotely', 'glassdoor', 'indeed', 'linkedin', 'rubyonrem
 COLUMN_NAME = ["job_title", "company_name", "address", "job_description", 'job_source_url', "job_posted_date", "salary_format", "estimated_salary", "salary_min", "salary_max", "job_source", "job_type", "job_description_tags"]
 REMOTE = 'remote'
 SALARY_FORMAT = '$'
+PIA_EXTENSION_PATH = 'scraper/utils/pia.crx'
+HIDE_ELEMENTS = {
+                'plugins': 2, 'popups': 2, 'geolocation': 2, 'notifications': 2,
+                'auto_select_certificate': 2, 'fullscreen': 2, 'mouselock': 2, 'mixed_script': 2,
+                'media_stream': 2, 'media_stream_mic': 2, 'media_stream_camera': 2,
+                'protocol_handlers': 2, 'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2,
+                'push_messaging': 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop': 2,
+                'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement': 2, 'durable_storage': 2
+                }
+
+
--- scraper/jobs/just_remote_scraping.py
+++ scraper/jobs/just_remote_scraping.py
@@ -1,3 +1,4 @@
+
 from logging import exception
 from datetime import timedelta, datetime
 import pandas as pd
@@ -5,139 +6,120 @@ from selenium.webdriver.common.by import By
 from scraper.constants.const import *
 from scraper.models.scraper_logs import ScraperLogs
 from scraper.utils.helpers import generate_scraper_filename, ScraperNaming, configure_webdriver, set_job_type, sleeper, previous_jobs
-from utils.helpers import saveLogs,log_scraper_running_time
+from utils.helpers import saveLogs, log_scraper_running_time
+from scraper.jobs.base_scraper import BaseScraper
 
 
-def load_jobs(driver):
-    previous_jobs = driver.find_elements(By.CLASS_NAME,"hxecsD")
-    previous_len = len(previous_jobs)
-    while True:
-        if not check_if_job_day_ago(previous_jobs[previous_len-1]):
-            break
-        sleeper()
-        driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-        previous_jobs = driver.find_elements(By.CLASS_NAME, "hxecsD")
-        if previous_len == len(previous_jobs):
-            break
+class JustRemoteScraper(BaseScraper):
+    def __init__(self, url: str):
+        super().__init__(url=url)
+
+    def load_jobs(self):
+        self.driver.get(self.url)
+        previous_jobs = self.get_element(locator='hxecsD', alls=True)
         previous_len = len(previous_jobs)
-
-    return previous_jobs
-
-def check_if_job_day_ago(job):
-    day_ago = False
-    try:
-        date_elm = job.find_element(By.CLASS_NAME, "new-job-item__JobItemDate-sc-1qa4r36-5").text
-        date_obj = datetime.strptime(date_elm, "%d %b")
-        current_year = datetime.now().year
-        date_obj = date_obj.replace(year=current_year)
-        current_date = datetime.now()
-        one_day_ago_date = current_date - timedelta(days=1)
-        if date_obj >= one_day_ago_date:
-            day_ago = True
-    except Exception as e:
-        saveLogs(e)
-    return day_ago
-
-def get_job_urls(driver):
-    job_urls = load_jobs(driver)
-    links = []
-    for job_url in job_urls:
-        if not check_if_job_day_ago(job_url):
-            continue
-        link = job_url.find_element(By.TAG_NAME, "a")
-        t = job_url.find_element(
-            By.CLASS_NAME, "new-job-item__JobItemDate-sc-1qa4r36-5")
-        links.append([link.get_attribute("href"), t.text])
-    return links
-
-
-def request_url(driver, url):
-    driver.get(url)
-
-
-# append data for csv file
-def append_data(data, field):
-    data.append(str(field).strip("+"))
-
-
-def find_jobs(driver):
-    scrapped_data = []
-    links = get_job_urls(driver)
-    exsiting_links = []
-    for link in links:
-        exsiting_links.append(link[0])
-    existing_jobs = previous_jobs(source=ScraperNaming.JUST_REMOTE, urls=exsiting_links)
-    original_window = driver.current_window_handle
-    for link in links:
-        data = []
-        if existing_jobs.get(link[0]):
-            continue
-        try:
-            driver.switch_to.new_window('tab')
-            driver.get(link[0])
+        while True:
+            if not self.check_if_job_day_ago(previous_jobs[previous_len-1]):
+                break
             sleeper()
-            temp = driver.find_element(
-                By.CLASS_NAME, "JpOdR")
-            job_title = temp.find_element(
-                By.CLASS_NAME, "job-title__StyledJobTitle-sc-10irtcq-0")
-            job_type_check = driver.find_elements(By.CLASS_NAME, "job-meta__Wrapper-oh0pn7-0")[0].text.lower()
-            append_data(data, job_title.text)
-            company_name = driver.find_element(
-                    By.CLASS_NAME, "ktyJgG")
-            append_data(data, company_name.text.splitlines()[0])
-            address = 'Remote'
-            append_data(data, address)
-            job_source_url = link[0]
-            append_data(data, job_source_url)
-            job_description = temp
-            append_data(data, job_description.text)
-            job_posted_date = link[1]
-            append_data(data, job_posted_date)
-            salary_format = "N/A"
-            append_data(data, salary_format)
-            salary_min = "N/A"
-            append_data(data, salary_min)
-            salary_max = "N/A"
-            append_data(data, salary_max)
-            estimated_salary = 'N/A'
-            append_data(data, estimated_salary)
-            job_source = ScraperNaming.JUST_REMOTE
-            append_data(data, job_source)
-            if 'contract' in job_type_check:
-                append_data(data, set_job_type('contract'))
-            elif 'permanent' in job_type_check:
-                append_data(data, set_job_type('full time'))
-            else:
+            self.driver.execute_script(
+                'window.scrollTo(0,document.body.scrollHeight)')
+            previous_jobs = self.get_element(locator='hxecsD', alls=True)
+            if previous_len == len(previous_jobs):
+                break
+            previous_len = len(previous_jobs)
+
+        return previous_jobs
+
+    def check_if_job_day_ago(self, job):
+        day_ago = False
+        try:
+            date_elm = self.get_element(
+                locator='new-job-item__JobItemDate-sc-1qa4r36-5', parent=job).text
+            date_obj = datetime.strptime(date_elm, "%d %b")
+            current_year = datetime.now().year
+            date_obj = date_obj.replace(year=current_year)
+            current_date = datetime.now()
+            one_day_ago_date = current_date - timedelta(days=1)
+            if date_obj >= one_day_ago_date:
+                day_ago = True
+        except Exception as e:
+            saveLogs(e)
+        return day_ago
+
+    def get_job_urls(self):
+        job_elements = self.load_jobs()
+        links = []
+        for job_element in job_elements:
+            if not self.check_if_job_day_ago(job_element):
                 continue
+            link = self.get_element(
+                selector='tag', locator='a', parent=job_element)
+            t = self.get_element(
+                locator="new-job-item__JobItemDate-sc-1qa4r36-5", parent=job_element)
+            links.append([link.get_attribute("href"), t.text])
+        return links
 
-            job_description_tags = temp.get_attribute("innerHTML")
-            append_data(data, str(job_description_tags))
-        except exception as e:
+    def find_jobs(self):
+        links = self.get_job_urls()
+        curr_links = []
+        for link in links:
+            curr_links.append(link[0])
+        self.get_previous_jobs(
+            job_source=ScraperNaming.JUST_REMOTE, urls=curr_links)
+        original_window = self.driver.current_window_handle
+        for link in links:
+            data = []
+            if self.previous_links.get(link[0]):
+                continue
+            try:
+                self.driver.switch_to.new_window('tab')
+                self.driver.get(link[0])
+                sleeper()
+                job_type_check = self.get_element(
+                    locator='job-meta__Wrapper-oh0pn7-0', alls=True)[0].text.lower()
+                if 'contract' in job_type_check:
+                    self.job['job_type'] = set_job_type('contract')
+                elif 'permanent' in job_type_check:
+                    self.job['job_type'] = set_job_type('full time')
+                else:
+                    continue
+                temp = self.get_element(
+                    locator='JpOdR')
+                job_title = self.get_element(
+                    locator='job-title__StyledJobTitle-sc-10irtcq-0', parent=temp)
+                self.job['job_title'] = job_title.text
+                company_name = self.get_element(
+                    locator='ktyJgG')
+                self.job['company_name'] = company_name.text.splitlines()[0]
+                self.job['address'] = 'Remote'
+                self.job['job_source_url'] = link[0]
+                self.job['job_description'] = temp.text
+                self.job['job_posted_date'] = link[1]
+                self.job['salary_format'] = 'N/A'
+                self.job['estimated_salary'] = 'N/A'
+                self.job['salary_min'] = 'N/A'
+                self.job['salary_max'] = 'N/A'
+                self.job['job_source'] = ScraperNaming.JUST_REMOTE
+                self.job['job_description_tags'] = temp.get_attribute(
+                    'innerHTML')
+            except exception as e:
                 saveLogs(e)
-        driver.close()
-        driver.switch_to.window(original_window)
-        scrapped_data.append(data)
-    if len(scrapped_data) > 0:
-        export_to_excel(scrapped_data)
-    
-def export_to_excel(scrapped_data):
-    df = pd.DataFrame(data=scrapped_data, columns=COLUMN_NAME)
-    filename = generate_scraper_filename(ScraperNaming.JUST_REMOTE)
-    df.to_excel(filename, index=False)
-
-    ScraperLogs.objects.create(
-        total_jobs=len(df), job_source="Just Remote", filename=filename
-    )
+            self.driver.close()
+            self.driver.switch_to.window(original_window)
+            self.scraped_jobs.append(self.job)
+            self.job = {}
+        if len(self.scraped_jobs) > 0:
+            self.export_to_excel(ScraperNaming.JUST_REMOTE, 'Just Remote')
 
 
-# code starts from here 
-@log_scraper_running_time("JustRemote")
+# code starts from here
+@log_scraper_running_time('JustRemote')
 def just_remote(link, job_type):
-    driver = configure_webdriver()
     try:
-        driver.maximize_window()
-        request_url(driver, link)
-        find_jobs(driver)
+        justremote_scraper = JustRemoteScraper(url=link)
+        justremote_scraper.find_jobs()
     except Exception as e:
         saveLogs(e)
-    driver.quit()
+    finally:
+        justremote_scraper.delete_self()
